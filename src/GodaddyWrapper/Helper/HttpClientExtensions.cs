558ec52b54db3b8fe60b74224b4e15c3291f8b0f
Adam Humpherys <adam@adamh.us>
2021-12-31 15:52:42 -0700

    JSON bugfix; resolves #19

--- src/GodaddyWrapper/Helper/HttpClientExtensions.cs
+++ src/GodaddyWrapper/Helper/HttpClientExtensions.cs
@@ -10,21 +10,18 @@ namespace GodaddyWrapper.Helper
 {
     internal static class HttpClientExtensions
     {
-        public static Task<HttpResponseMessage> PostAsync(this HttpClient client, string requestUri, Object content)
+        public static Task<HttpResponseMessage> PostAsync(this HttpClient client, string requestUri, string jsonString)
         {
-            var jsonString = JsonConvert.SerializeObject(content, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
             return client.PostAsync(requestUri, new StringContent(jsonString, Encoding.UTF8, "application/json"));
         }
 
-        public static Task<HttpResponseMessage> PutAsync(this HttpClient client, string requestUri, Object content)
+        public static Task<HttpResponseMessage> PutAsync(this HttpClient client, string requestUri, string jsonString)
         {
-			var jsonString = JsonConvert.SerializeObject(content, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 			return client.PutAsync(requestUri, new StringContent(jsonString, Encoding.UTF8, "application/json"));
         }
 
-        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, Object content)
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, string jsonString)
         {
-            var jsonString = JsonConvert.SerializeObject(content, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
             var method = new HttpMethod("PATCH");
             var request = new HttpRequestMessage(method, requestUri){
                 Content = new StringContent(jsonString, Encoding.UTF8, "application/json")
