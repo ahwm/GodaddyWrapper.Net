f2dd19cb26e092491787db261011187e29fd4fcf
Adam <adam@adamh.us>
2025-10-23 10:58:17 -0600

    build fix

--- src/GodaddyWrapper.Tests/Startup.cs
+++ src/GodaddyWrapper.Tests/Startup.cs
@@ -11,9 +11,9 @@ namespace GodaddyWrapper.Tests
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            string AccessKey = Environment.GetEnvironmentVariable("GODADDY_ACCESS_KEY").Trim();
-            string ApiSecret = Environment.GetEnvironmentVariable("GODADDY_API_SECRET").Trim();
-            services.AddGoDaddy(AccessKey, ApiSecret, true);
+            //string AccessKey = Environment.GetEnvironmentVariable("GODADDY_ACCESS_KEY").Trim();
+            //string ApiSecret = Environment.GetEnvironmentVariable("GODADDY_API_SECRET").Trim();
+            //services.AddGoDaddy(AccessKey, ApiSecret, true);
             //services.AddTransient<IDomainService, OpenSRSDomainService>();
         }
     }
